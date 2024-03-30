04ac81695ac9c7815808bc5e46f47a58fdcad23b
Åke Ek <svein.ek@stud.noroff.no>
2024-03-30 19:58:23 +0100

    Routing style fixes

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var aboutRouter = require('./routes/about');
-var servicesRouter = require('./routes/services');
-var recommendationsRouter = require('./routes/recommendations');
-var portfolioRouter = require('./routes/portfolio');
-var contactRouter = require('./routes/contact');
+var indexRouter = require('./public/routes/index');
+var usersRouter = require('./public/routes/users');
+var homeRouter = require('./public/routes/home');
+var aboutRouter = require('./public/routes/about');
+var servicesRouter = require('./public/routes/services');
+var recommendationsRouter = require('./public/routes/recommendations');
+var portfolioRouter = require('./public/routes/portfolio');
+var contactRouter = require('./public/routes/contact');
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use(express.static(__dirname + '/node_modules/typed.js/lib'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/home', homeRouter);
 app.use('/about', aboutRouter);
 app.use('/services', servicesRouter);
 app.use('/recommendations', recommendationsRouter);
